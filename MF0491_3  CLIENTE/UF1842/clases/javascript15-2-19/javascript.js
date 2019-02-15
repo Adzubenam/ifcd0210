ca58609af23aff21978d2ad153766c900aad7728
adzubenam <manebuzda@gmail.com>
2019-02-15 20:32:38 +0000

    viernes 15 de febrero de 2019

---
+++ MF0491_3  CLIENTE/UF1842/clases/javascript15-2-19/javascript.js
@@ -0,0 +1,13 @@
+alert("hola mundo con javascript");
+alert("bienvenidos a javascript");
+document.write("hola mundo desde fichero externo");
+console.log("muestra esto en la consola");
+console.log(88+4);
+//comentario
+/*comentario
+*/comentario 
+//variables
+var pais = "españa";
+var continente = "europa";
+var antigüedad = 2019;
+console.log(pais, contiente, antigüedad)
